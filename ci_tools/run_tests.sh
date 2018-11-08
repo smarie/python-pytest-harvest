6eec31b90665149a8f32292d60ce1fb6ca0f08e2
Sylvain Marie <sylvain.marie@schneider-electric.com>
2018-11-08 16:55:53 +0100

    Attempt to fix coverage and run more tests

--- ci_tools/run_tests.sh
+++ ci_tools/run_tests.sh
@@ -11,4 +11,11 @@ cleanup() {
 }
 
 trap "cleanup" INT TERM EXIT
+
+COVERAGE_FILE=".coverage.meta"
 pytest --junitxml=reports/junit/junit.xml --html=reports/junit/report.html --cov-report term-missing --cov=./pytest_harvest -v pytest_harvest/tests/
+
+COVERAGE_FILE=".coverage.raw"
+pytest --cov-report term-missing --cov=./pytest_harvest -v pytest_harvest/tests_raw/
+
+coverage combine
