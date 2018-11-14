5971b5b290702048a3e8664904fd36f7ae588b4e
Sylvain Marie <sylvain.marie@schneider-electric.com>
2018-11-14 16:13:02 +0100

    Added pandas and tabulate in the test requirements

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ here = path.abspath(path.dirname(__file__))
 INSTALL_REQUIRES = ['decorator', 'funcsigs;python_version<"3.3"']
 DEPENDENCY_LINKS = []
 SETUP_REQUIRES = ['pytest-runner', 'setuptools_scm', 'pypandoc', 'pandoc']
-TESTS_REQUIRE = ['pytest', 'pytest-logging', 'pytest-cov', 'pytest-steps']
+TESTS_REQUIRE = ['pytest', 'pytest-logging', 'pytest-cov', 'pytest-steps', 'pandas', 'tabulate']
 EXTRAS_REQUIRE = {}
 
 # simple check
