167cf485b94f2c96e8218e8decaeafd7e3b49e4b
Sylvain Marie <sylvain.marie@schneider-electric.com>
2018-11-20 22:15:04 +0100

    Improving coverage report for test that are meta-tested

--- ci_tools/run_tests.sh
+++ ci_tools/run_tests.sh
@@ -18,9 +18,10 @@ trap "cleanup" INT TERM EXIT
 #    echo "pytest not found. Trying py.test"
 #fi
 
-# First the raw
+# First the raw for coverage
 echo -e "\n\n****** Running tests : 1/2 RAW******\n\n"
-python -m pytest --cov-report term-missing --cov=./pytest_harvest -v pytest_harvest/tests_raw/
+coverage run --source pytest_harvest -m pytest -v pytest_harvest/tests_raw/
+#python -m pytest --cov-report term-missing --cov=./pytest_harvest -v pytest_harvest/tests_raw/
 
 # Then the meta (appended)
 echo -e "\n\n****** Running tests : 2/2 META******\n\n"
